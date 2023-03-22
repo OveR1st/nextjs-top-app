cd7bf91798d71e8cf58e023d6d94e2f1303b276b
Dmitriy Telniy <dmitriy.telniy@ideus.biz>
2023-03-22 20:58:12 +0200

    Init fonts and css global var

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,21 @@ import Head from 'next/head';
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>My-top-app</title>
 
         <link key={1} rel="icon" href="/favicon.ico" />
-      </Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head> */}
       <Component {...pageProps} />;
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,21 @@ export default function Document(): JSX.Element {
   // }
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>My-top-app</title>
+
+        <link key={1} rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
