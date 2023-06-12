85258d6a6af8d042ce76eeb8bb4eb1e2b0746452
Dmitriy Telniy <dmitriy.telniy@ideus.biz>
2023-06-12 21:39:12 +0300

    grid layout footer and date-fns

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 
-export const Footer: React.FC<FooterProps> = ({ ...props }): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer: React.FC<FooterProps> = ({
+  className,
+  ...props
+}): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>OwlTop © 2023 - {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
